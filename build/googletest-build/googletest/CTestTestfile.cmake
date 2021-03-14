056407923fe3c4609d70115dc389f3233933169a
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2021-03-14 21:54:59 +0100

    removed Cmake build files

--- build/googletest-build/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: G:/prog_perso/C++/LexerGenerator/build/googletest-src/googletest
-# Build directory: G:/prog_perso/C++/LexerGenerator/build/googletest-build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
